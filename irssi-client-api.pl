684c97e7c09e0df92ea8e9e91a1f8e97560218a3
Axel Eirola <axel.eirola@iki.fi>
2013-01-21 20:12:43 +0200

    Added loadable irssi script skeleton.

---
+++ irssi-client-api.pl
@@ -0,0 +1,15 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use Irssi;
+$VERSION = '0.01';
+%IRSSI = (
+    authors     => 'Axel Eirola',
+    contact     => 'axel.eirola@iki.fi',
+    name        => 'irssi client api',
+    description => 'This script allows ' .
+                   'remote clients to  ' .
+                   'control irssi.',
+    license     => 'Public Domain',
+);
+
