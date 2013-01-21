0d5e53089c4f369bdbc179152f4f01cf427f4a1e
Axel Eirola <axel.eirola@iki.fi>
2013-01-21 22:47:22 +0200

    Added basic plumbing for handling clients.

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ Plugin and clients for remotely operating irssi.
 Extending the idea of the irssi-notify script to enable two-way communication and interaction. This is like an combination of irssi-connectbot and irssi-notify, the idea is to make it as easy as possible to operate irc from mobile hardware platforms.
 
 The irssi-script listens to a TCP port or pipe or fifo file or something and provides an API for clients connecting to the script. Over the API the client can fetch information on windows and their content, as well as send commands to irssi.
+
+At the moment the client listens to port 10000, and reacts to the following commands:
+* `windows`: lists all open windows
+* `active_window`: lists active window
+* `bye`: closes the connection
+
+Usage example
+-------------
+* `/script load irssi-client-api`
+* `nc localhost 10000`
+* `windows`
+* `bye`
+
+
+Known issues
+------------
+* Freezes irssi on connection close
+* Probably leaks connections or something
+* Doesn't do much yet
+* Might eat a lot of cpu at times?
--- irssi-client-api.pl
+++ irssi-client-api.pl
@@ -1,7 +1,13 @@
+# irssi-client-api.pl -- enables remote control of irssi
+
 use strict;
+
+use Irssi;      # For interfacign with irssi
+use IO::Socket; # For TCP connections
+use JSON;       # For priducing JSON output
+
 use vars qw($VERSION %IRSSI);
 
-use Irssi;
 $VERSION = '0.01';
 %IRSSI = (
     authors     => 'Axel Eirola',
@@ -12,4 +18,82 @@ $VERSION = '0.01';
                    'control irssi.',
     license     => 'Public Domain',
 );
+
+our ( $server );
+
+sub setup {
+    print "Setting up";
+    $server = IO::Socket::INET->new(LocalPort => 10000,
+                                    Type      => SOCK_STREAM,
+                                    Reuse     => 1,
+                                    Listen    => 10 )   # or SOMAXCONN
+        or die "Couldn't be a tcp server on port 100000 : $@\n";
+    
+    my @args = ($server);
+    print "Server at " . fileno($server);
+    # Add handler for server connections
+    Irssi::input_add(fileno($server),
+                        Irssi::INPUT_READ,
+                        \&handle_connection,
+                        \@args);
+}   
+
+sub handle_connection {
+    my $args = shift;
+    my ($server) = @$args;
+    my $client = $server->accept();
+    
+    print "Client connected at" . fileno($client);
+    print $client "You have connected to irssi-client-script\n";
+    
+    my @args = ($client);
+    # Add handler for client messages
+    Irssi::input_add(fileno($client),
+                        Irssi::INPUT_READ,
+                        \&handle_message,
+                        \@args);
+}
+
+sub handle_message {
+    my $args = shift;
+    my ($client) = @$args;
+    
+    my $msg;
+    $client->recv($msg, 1024);
+    
+    if ($msg =~ /^windows$/) {
+        foreach (Irssi::windows()) {
+            print $client $_->{refnum} . " " . $_->{name} . "\n";
+        }
+    } elsif ($msg =~ /^active_window$/) {
+        my $win = Irssi::active_win();
+        print $client $win->{refnum} . " " . $win->{name} . "\n";
+    } elsif ($msg =~ /^bye$/) {
+        print $client "Thankyoucomeagain\n";
+        close($client);
+        Irssi::input_remove(fileno($client));
+    } else {
+        print $client "echo: " . $msg;
+    }
+}
+
+sub teardown {
+    Irssi::input_remove(fileno($server));
+    close($server);
+}
+
+# Setup on load
+setup();
+
+# Teardown on unload
+Irssi::signal_add_first
+    'command script unload', sub {
+        my ($script) = @_;
+        return unless $script =~
+            /(?:^|\s) $IRSSI{name}
+             (?:\.[^. ]*)? (?:\s|$) /x;
+        teardown();
+        Irssi::print("%B>>%n $IRSSI{name} $VERSION unloaded", MSGLEVEL_CLIENTCRAP);
+    };    
+print CLIENTCRAP "%B>>%n $IRSSI{name} $VERSION (by $IRSSI{authors}) loaded";
 
